35e152bcd076b101e053e3d37d2d78b9a3fb29c8
Brandon Benefield <bsquared18@gmail.com>
2019-08-07 22:39:25 -0400

    PROTOTYPE COMPLETE

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
