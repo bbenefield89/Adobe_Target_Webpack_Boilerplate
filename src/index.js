4ceedd30aeb184bdd232ca08d2e244871ef2d841
Brandon Benefield <bsquared18@gmail.com>
2019-08-30 18:52:21 -0400

    DELETE OLD TESTING DIR/FILES
    - ./src

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-import tgtElemReady from './MutationObserver'
-
-tgtElemReady('body', bodyEl => {
-  console.log('\n\n\n\nWEEEEEEEEEEEEEOOOO\n\n\n\n')
-  console.log(bodyEl)
-}, '${campaign.name}')
