c47ef1f30d322b41e765228077e3330b7962a3aa
Brandon Benefield <bsquared18@gmail.com>
2019-08-30 18:53:24 -0400

    ADD DYNAMIC ENTRY/OUTPUT PATHS
    - webpack.config.js
      - add plugin script to wrap minified scripts in an HTML <script> tag to be placed in Adobe Target

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,53 @@
-const path = require('path');
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: glob.sync('./abtests/**/index.js').reduce((acc, path) => {
+        const entry = path.replace('/index.js', '')
+        acc[entry] = path
+        return acc
+    }, {}),
+
+    output: {
+        filename: './[name]/build/build.js',
+        path: path.resolve(__dirname)
+    },
+
+    mode: 'production',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ]
-      }
+    },
+
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                    Object.keys(compilation.assets).forEach(entry => {
+                        fs.readFile(entry, 'utf-8', (err, data) => {
+                            if (err) {
+                                console.log('\n\n\nRead File Err: ', err, '\n\n\n')
+                                return
+                            }
+                            
+                            fs.writeFile(entry, `<script>${data}</script>`, err => {
+                                if (err) {
+                                    console.log('\n\n\nWrite File Err: ', err, '\n\n\n')
+                                }
+                            })
+                        })
+                    })
+                });
+            }
+        }
     ]
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+}
